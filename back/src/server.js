cfc0da737c5b447f8326e221f36974907702282f
sudo-ramonkey <j.ramonroz@gmail.com>
2025-06-06 17:54:52 -0600

    fix: remove debug log

--- back/src/server.js
+++ back/src/server.js
@@ -8,7 +8,6 @@ const productosRoutes = require("./routes/productos");
 const app = express();
 const PORT = process.env.PORT || 4000;
 const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/fs1-mern";
-console.log("Using MongoDB:", MONGO_URI);
 
 // Middleware
 app.use(cors());
